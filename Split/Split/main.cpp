df801a5180b1a0b230718fb921e71c54f0714a54
Ambrish Parekh <parekha@usc.edu>
2019-11-21 13:14:05 -0800

    initial Xcode files

---
+++ Split/Split/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Split
+//
+//  Created by Ambrish Parekh on 11/21/19.
+//  Copyright © 2019 Ambrish Parekh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
